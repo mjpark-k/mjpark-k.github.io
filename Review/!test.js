54be910e9c44878c360aee75cbb27b6a909c899b
mjpark-k <pmj9498@gmail.com>
2024-12-08 17:08:18 +0900

    review day1

---
+++ Review/!test.js
@@ -0,0 +1,11 @@
+function printArguments(...args) {
+  // for (const arg of args) {
+  //   console.log(arg);
+  // }
+  console.log(args);
+  console.log('----------------');
+}
+
+printArguments('Youg', 'Mark', 'Koby');
+printArguments('Captain');
+printArguments('Suri', 'Jack', 'Joy', 'Noel');
